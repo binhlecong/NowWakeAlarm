0838a00ce333a77c558c81dad9a4979b48653907
binhlecong <congbinh21012001@gmail.com>
2021-11-18 22:30:49 +0700

    Initial commit

---
+++ app/src/main/java/com/hcmus_csc13009/nowwakealarm/models/Alarm.java
@@ -0,0 +1,4 @@
+package com.hcmus_csc13009.nowwakealarm.models;
+
+public class Alarm {
+}
---
+++ app/src/main/java/com/hcmus_csc13009/nowwakealarm/ui/MainActivity.java
@@ -0,0 +1,16 @@
+package com.hcmus_csc13009.nowwakealarm.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.hcmus_csc13009.nowwakealarm.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

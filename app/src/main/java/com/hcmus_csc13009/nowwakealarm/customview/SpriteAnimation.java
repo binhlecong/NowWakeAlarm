1ab1824e385ac46deebdb856142a6464ae7fadf3
binhlecong <congbinh21012001@gmail.com>
2021-11-21 01:07:32 +0700

    add handle alarm challenge

---
+++ app/src/main/java/com/hcmus_csc13009/nowwakealarm/customview/SpriteAnimation.java
@@ -0,0 +1,92 @@
+package com.hcmus_csc13009.nowwakealarm.customview;
+
+import android.animation.AnimatorSet;
+import android.animation.ValueAnimator;
+import android.content.Context;
+import android.graphics.Bitmap;
+
+import androidx.appcompat.widget.AppCompatImageView;
+
+public class SpriteAnimation extends AppCompatImageView {
+    /*
+     * bitmap: whole sprite sheet
+     * posX: X position of sprite in sprite sheet
+     * posY: Y position of sprite in sprite sheet
+     * width: width of sprite
+     * height: height of sprite
+     * columns: number of sprite frame
+     * duration: time to play sprite animation
+     */
+    final private Bitmap bitmap;
+    final private int width, height;
+    ValueAnimator sprite;
+    AnimatorSet animationSet;
+    private int posX, posY;
+    private int columns; // number of sprite frame
+    private int frameIndex; // current frame in sprite animation
+
+    SpriteAnimation(Context context, Bitmap bitmap, int posX, int posY, int width, int height,
+                    int columns, long spriteDuration) {
+        super(context, null);
+        this.bitmap = bitmap;
+        this.columns = columns;
+        this.width = width;
+        this.height = height;
+        this.frameIndex = 0;
+        this.posX = posX;
+        this.posY = posY;
+        animationSet = new AnimatorSet();
+        this.setImageBitmap(getSubImage(posX, posY, width, height));
+        // setup sprite animation
+        sprite = ValueAnimator.ofInt(0, columns - 1);
+        sprite.setDuration(spriteDuration);
+        sprite.setRepeatCount(ValueAnimator.INFINITE);
+        sprite.addUpdateListener(valueAnimator -> {
+            int id = (Integer) valueAnimator.getAnimatedValue();
+            if (id == this.frameIndex) return;
+            this.frameIndex = id;
+            int x = posX + id * this.width;
+            SpriteAnimation.this.setImageBitmap(
+                    SpriteAnimation.this.getSubImage(x, this.posY, this.width, this.height));
+            invalidate();
+        });
+        animationSet.play(sprite);
+    }
+
+    // default duration = 40*frames (millis)
+    public SpriteAnimation(Context context, Bitmap bitmap, int posX, int posY, int width,
+                           int height,
+                           int columns) {
+        this(context, bitmap, posX, posY, width, height, columns, 40L * columns);
+    }
+
+    private Bitmap getSubImage(int x, int y, int width, int height) {
+        return Bitmap.createBitmap(this.bitmap, x, y, width, height);
+    }
+
+    // use this to add another animation
+    public AnimatorSet getAnimationSet() {
+        return animationSet;
+    }
+
+    public void startAnimation() {
+        animationSet.start();
+    }
+
+    public void cancelAnimation() {
+        animationSet.cancel();
+    }
+
+    public void setPosX(int x) {
+        this.posX = x;
+    }
+
+    public void setPosY(int y) {
+        this.posY = y;
+    }
+
+    public void setColumns(int columns) {
+        this.columns = columns;
+        sprite.setIntValues(0, this.columns - 1);
+    }
+}
--- app/src/main/java/com/hcmus_csc13009/nowwakealarm/ui/HandleAlarmActivity.java
+++ app/src/main/java/com/hcmus_csc13009/nowwakealarm/ui/HandleAlarmActivity.java
@@ -2,21 +2,32 @@ package com.hcmus_csc13009.nowwakealarm.ui;
 
 import static com.hcmus_csc13009.nowwakealarm.utils.AlarmUtils.cancelAlarm;
 
+import android.animation.Animator;
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.WindowManager;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.hcmus_csc13009.nowwakealarm.R;
+import com.hcmus_csc13009.nowwakealarm.customview.SpriteAnimation;
 import com.hcmus_csc13009.nowwakealarm.models.Alarm;
 import com.hcmus_csc13009.nowwakealarm.service.AlarmService;
 import com.hcmus_csc13009.nowwakealarm.viewmodel.AlarmViewModel;
 
+import java.util.Random;
+
 public class HandleAlarmActivity extends AppCompatActivity {
+    int cnt = 3;
     private Alarm alarm;
+    private ConstraintLayout mainLayout;
     private AlarmViewModel alarmsListViewModel;
 
     @Override
@@ -39,6 +50,9 @@ public class HandleAlarmActivity extends AppCompatActivity {
         Bundle bundle = getIntent().getBundleExtra(getString(R.string.bundle_alarm_obj));
         if (bundle != null)
             alarm = (Alarm) bundle.getSerializable(getString(R.string.arg_alarm_obj));
+        // Run animations
+        mainLayout = findViewById(R.id.mainConstraintLayout);
+        doSomething();
     }
 
     private void dismissAlarm() {
@@ -51,5 +65,80 @@ public class HandleAlarmActivity extends AppCompatActivity {
         Intent intentService = new Intent(getApplicationContext(), AlarmService.class);
         getApplicationContext().stopService(intentService);
         finish();
+    }
+
+    void doSomething() {
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inScaled = false;
+        Bitmap bmp = BitmapFactory.decodeResource(getResources(), R.drawable.bird_sprite, options);
+        bmp = Bitmap.createScaledBitmap(bmp, 640, 96, false);
+
+        for (int i = 0; i < cnt; ++i) {
+            SpriteAnimation x = new SpriteAnimation(this, bmp, 0, 0, 80, 96, 8);
+            x.setX(200 * i);
+            x.setY(50 + 100 * i);
+            x.setOnClickListener(view -> x.setY(0));
+            Random random = new Random();
+            ObjectAnimator translateX = new ObjectAnimator();
+            translateX.setTarget(x);
+            translateX.setPropertyName("x");
+            translateX.setFloatValues(400f);
+            translateX.setDuration((i + 1) * 1000);
+            translateX.setRepeatMode(ValueAnimator.RESTART);
+            translateX.setRepeatCount(ValueAnimator.INFINITE);
+            translateX.addListener(new Animator.AnimatorListener() {
+                @Override
+                public void onAnimationStart(Animator animator) {
+                }
+
+                @Override
+                public void onAnimationEnd(Animator animator) {
+                }
+
+                @Override
+                public void onAnimationCancel(Animator animator) {
+                }
+
+                @Override
+                public void onAnimationRepeat(Animator animator) {
+                    int prev = Math.round((Float) translateX.getAnimatedValue());
+                    int nextValue = random.nextInt(800);
+                    translateX.setFloatValues(prev, nextValue);
+                }
+            });
+            ObjectAnimator translateY = ObjectAnimator.ofFloat(x, "y", 800f);
+            translateY.setDuration((i + 1) * 1000);
+            translateY.setRepeatCount(ValueAnimator.INFINITE);
+            translateY.addListener(new Animator.AnimatorListener() {
+                @Override
+                public void onAnimationStart(Animator animator) {
+                }
+
+                @Override
+                public void onAnimationEnd(Animator animator) {
+                }
+
+                @Override
+                public void onAnimationCancel(Animator animator) {
+                }
+
+                @Override
+                public void onAnimationRepeat(Animator animator) {
+                    int prev = Math.round((Float) translateY.getAnimatedValue());
+                    int nextValue = random.nextInt(1600);
+                    translateY.setFloatValues(prev, nextValue);
+                }
+            });
+            x.getAnimationSet().playTogether(translateX, translateY);
+            x.getAnimationSet().start();
+            // Add listener to sprite
+            x.setOnClickListener(v -> {
+                cnt--;
+                if (cnt == 0) dismissAlarm();
+                mainLayout.removeView(v);
+            });
+
+            mainLayout.addView(x);
+        }
     }
 }

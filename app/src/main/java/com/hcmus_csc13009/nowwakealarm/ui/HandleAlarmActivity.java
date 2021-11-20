0f6e06da9794ab47371d17be152158325403a42a
binhlecong <congbinh21012001@gmail.com>
2021-11-20 21:28:52 +0700

    Add activity to end alarm

--- app/src/main/java/com/hcmus_csc13009/nowwakealarm/ui/HandleAlarmActivity.java
+++ app/src/main/java/com/hcmus_csc13009/nowwakealarm/ui/HandleAlarmActivity.java
@@ -1,16 +1,55 @@
 package com.hcmus_csc13009.nowwakealarm.ui;
 
-import androidx.appcompat.app.AppCompatActivity;
+import static com.hcmus_csc13009.nowwakealarm.utils.AlarmUtils.cancelAlarm;
 
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.WindowManager;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
 
 import com.hcmus_csc13009.nowwakealarm.R;
+import com.hcmus_csc13009.nowwakealarm.models.Alarm;
+import com.hcmus_csc13009.nowwakealarm.service.AlarmService;
+import com.hcmus_csc13009.nowwakealarm.viewmodel.AlarmViewModel;
 
 public class HandleAlarmActivity extends AppCompatActivity {
+    private Alarm alarm;
+    private AlarmViewModel alarmsListViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_handle_alarm);
+        // Lock screen, use full screen for displaying challenges
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O_MR1) {
+            setShowWhenLocked(true);
+            setTurnScreenOn(true);
+        } else {
+            getWindow().addFlags(
+                    WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON
+                            | WindowManager.LayoutParams.FLAG_ALLOW_LOCK_WHILE_SCREEN_ON
+            );
+        }
+        // Get ViewModel
+        alarmsListViewModel = new ViewModelProvider(this).get(AlarmViewModel.class);
+        // Get alarm from bundle
+        Bundle bundle = getIntent().getBundleExtra(getString(R.string.bundle_alarm_obj));
+        if (bundle != null)
+            alarm = (Alarm) bundle.getSerializable(getString(R.string.arg_alarm_obj));
+    }
+
+    private void dismissAlarm() {
+        if (alarm != null) {
+            alarm.setEnable(false);
+            cancelAlarm(getBaseContext(), alarm);
+            alarmsListViewModel.update(alarm);
+        }
+        // Exit activity and end alarm service
+        Intent intentService = new Intent(getApplicationContext(), AlarmService.class);
+        getApplicationContext().stopService(intentService);
+        finish();
     }
 }

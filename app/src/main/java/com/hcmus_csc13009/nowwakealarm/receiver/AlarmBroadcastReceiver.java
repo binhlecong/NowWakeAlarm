d69cc2befa580fce797af63994f1c6ada433144c
binhlecong <congbinh21012001@gmail.com>
2021-11-22 12:26:14 +0700

    get alarm location

--- app/src/main/java/com/hcmus_csc13009/nowwakealarm/receiver/AlarmBroadcastReceiver.java
+++ app/src/main/java/com/hcmus_csc13009/nowwakealarm/receiver/AlarmBroadcastReceiver.java
@@ -1,22 +1,79 @@
 package com.hcmus_csc13009.nowwakealarm.receiver;
 
+import android.Manifest;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.GpsStatus;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.widget.Toast;
 
+import androidx.core.app.ActivityCompat;
+
+import com.google.android.gms.maps.model.LatLng;
 import com.hcmus_csc13009.nowwakealarm.R;
 import com.hcmus_csc13009.nowwakealarm.models.Alarm;
 import com.hcmus_csc13009.nowwakealarm.service.AlarmService;
 import com.hcmus_csc13009.nowwakealarm.service.RescheduleAlarmService;
+import com.hcmus_csc13009.nowwakealarm.utils.MapUtil;
+import com.hcmus_csc13009.nowwakealarm.utils.SettingConstant;
 import com.hcmus_csc13009.nowwakealarm.utils.WeekDays;
 
 import java.util.Calendar;
 
 public class AlarmBroadcastReceiver extends BroadcastReceiver {
+    public static final int MIN_TIME_REQUEST = 5 * 1000;
+    private static Location currentLocation;
+    private static Location prevLocation;
+    private static LocationManager locationManager;
+    private static LocationListener locationListener = new LocationListener() {
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+            try {
+                String strStatus = "";
+                switch (status) {
+                    case GpsStatus.GPS_EVENT_FIRST_FIX:
+                        strStatus = "GPS_EVENT_FIRST_FIX";
+                        break;
+                    case GpsStatus.GPS_EVENT_SATELLITE_STATUS:
+                        strStatus = "GPS_EVENT_SATELLITE_STATUS";
+                        break;
+                    case GpsStatus.GPS_EVENT_STARTED:
+                        strStatus = "GPS_EVENT_STARTED";
+                        break;
+                    case GpsStatus.GPS_EVENT_STOPPED:
+                        strStatus = "GPS_EVENT_STOPPED";
+                        break;
+                    default:
+                        strStatus = String.valueOf(status);
+                        break;
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+        }
+
+        @Override
+        public void onLocationChanged(Location location) {
+        }
+    };
     Alarm alarm;
+    private String provider = LocationManager.GPS_PROVIDER;
+
 
     @Override
     public void onReceive(Context context, Intent intent) {
@@ -33,15 +90,56 @@ public class AlarmBroadcastReceiver extends BroadcastReceiver {
             Toast.makeText(context, toastText, Toast.LENGTH_SHORT).show();
 
             if (alarm != null) {
-                if (!alarm.isRepeatMode()) {
-                    startAlarmService(context, alarm);
+                if (alarm.getPosition() != null) {
+                    locationManager = (LocationManager) context
+                            .getSystemService(Context.LOCATION_SERVICE);
+                    if (locationManager.isProviderEnabled(provider)) {
+                        if (ActivityCompat.checkSelfPermission(context,
+                                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                                ActivityCompat.checkSelfPermission(context,
+                                        Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                            if (!alarm.isRepeatMode()) {
+                                startAlarmService(context, alarm);
+                            } else {
+                                if (isAlarmToday(alarm)) {
+                                    startAlarmService(context, alarm);
+                                }
+                            }
+                        } else {
+                            locationManager.requestLocationUpdates(provider,
+                                    MIN_TIME_REQUEST, 5, locationListener);
+                            Location loca = locationManager
+                                    .getLastKnownLocation(provider);
+                            LatLng dest = alarm.getLatLngPosition();
+                            if (MapUtil.getDistance(loca.getLatitude(), loca.getLongitude(),
+                                    dest.latitude, dest.longitude) < SettingConstant.NEARBY_RANGE) {
+                                startNotifyService(context, alarm);
+                            }
+                        }
+                    } else {
+                        if (!alarm.isRepeatMode()) {
+                            startAlarmService(context, alarm);
+                        } else {
+                            if (isAlarmToday(alarm)) {
+                                startAlarmService(context, alarm);
+                            }
+                        }
+                    }
                 } else {
-                    if (isAlarmToday(alarm)) {
+                    if (!alarm.isRepeatMode()) {
                         startAlarmService(context, alarm);
+                    } else {
+                        if (isAlarmToday(alarm)) {
+                            startAlarmService(context, alarm);
+                        }
                     }
                 }
             }
         }
+    }
+
+    private void startNotifyService(Context context, Alarm alarm) {
+
     }
 
     private boolean isAlarmToday(Alarm alarm1) {

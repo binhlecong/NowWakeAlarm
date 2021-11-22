4133cc265a8e1ab200ec7f5bab2f21b8315c5f48
Nguyễn Lê Bảo Thi <66853454+baothi250501@users.noreply.github.com>
2021-11-22 16:06:38 +0700

    Delete viewpager

--- app/src/main/java/com/hcmus_csc13009/nowwakealarm/ui/MainActivity.java
+++ app/src/main/java/com/hcmus_csc13009/nowwakealarm/ui/MainActivity.java
@@ -3,24 +3,13 @@ package com.hcmus_csc13009.nowwakealarm.ui;
 import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.Typeface;
-import android.net.Uri;
 import android.os.Bundle;
-import android.text.SpannableString;
-import android.text.Spanned;
-import android.text.style.ForegroundColorSpan;
 import android.view.Menu;
-import android.view.Window;
-import android.view.WindowManager;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentStatePagerAdapter;
 import androidx.fragment.app.FragmentTransaction;
-import androidx.fragment.app.ListFragment;
-import androidx.viewpager.widget.ViewPager;
-import androidx.viewpager2.widget.ViewPager2;
-import com.hcmus_csc13009.nowwakealarm.ui.MyViewPagerAdapter;
 
 import com.hcmus_csc13009.nowwakealarm.R;
 import com.wwdablu.soumya.lottiebottomnav.FontBuilder;
@@ -36,21 +25,14 @@ public class MainActivity extends AppCompatActivity implements ILottieBottomNavC
     FragmentTransaction transaction = null;
     LottieBottomNav bottomNav;
     ArrayList<MenuItem> list;
-    private ViewPager viewPager;
-    private MyViewPagerAdapter myViewPagerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //mViewPager2 = findViewById(R.id.myViewPager2);
 
         bottomNav   = findViewById(R.id.bottomAppBar);
-
-        //viewPager = findViewById(R.id.viewPager);
-        
-        //setUpViewPager();
 
 
         //Set font item
@@ -120,73 +102,28 @@ public class MainActivity extends AppCompatActivity implements ILottieBottomNavC
         setFragment(new HomeFragment());
     }
 
-    private void setUpViewPager() {
-        myViewPagerAdapter = new MyViewPagerAdapter(getSupportFragmentManager(), FragmentStatePagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
-        viewPager.setAdapter(myViewPagerAdapter);
-
-        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
-            @Override
-            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-
-            }
-
-            @Override
-            public void onPageSelected(int position) {
-                switch (position){
-                    case 0:
-                        setFragment(new HomeFragment());
-                        bottomNav.setSelectedIndex(0);
-                        break;
-                    case 1:
-                        setFragment(new AlarmsFragment());
-                        bottomNav.setSelectedIndex(1);
-                        break;
-                    case 2:
-                        setFragment(new MapFragment());
-                        bottomNav.setSelectedIndex(3);
-                        break;
-                    case 3:
-                        setFragment(new SettingsFragment());
-                        bottomNav.setSelectedIndex(4);
-                        break;
-
-                }
-            }
-
-            @Override
-            public void onPageScrollStateChanged(int state) {
-
-            }
-        });
-    }
-
     @Override
     public void onMenuSelected(int oldIndex, int newIndex, MenuItem menuItem) {
         switch (newIndex) {
             case 0: {
                 setFragment(new HomeFragment());
-                //viewPager.setCurrentItem(0);
                 break;
             }
             case 1: {
                 setFragment(new AlarmsFragment());
-                //viewPager.setCurrentItem(1);
                 break;
             }
             case 2: {
                 startActivity(new Intent(MainActivity.this, AddAlarmActivity.class));
                 bottomNav.setSelectedIndex(1);
-                //setFragment(new AlarmsFragment());
                 break;
             }
             case 3: {
                 setFragment(new MapFragment());
-                //viewPager.setCurrentItem(2);
                 break;
             }
             case 4: {
                 setFragment(new SettingsFragment());
-                //viewPager.setCurrentItem(3);
                 break;
             }
         }

75359f6c2cabfadb0e888685bd0796369eef249d
Nguyễn Lê Bảo Thi <66853454+baothi250501@users.noreply.github.com>
2021-11-22 11:43:38 +0700

    Update setting fragment

--- app/src/main/java/com/hcmus_csc13009/nowwakealarm/ui/SettingsFragment.java
+++ app/src/main/java/com/hcmus_csc13009/nowwakealarm/ui/SettingsFragment.java
@@ -1,21 +1,57 @@
 package com.hcmus_csc13009.nowwakealarm.ui;
 
+import android.content.Intent;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 
 import com.hcmus_csc13009.nowwakealarm.R;
+import com.hcmus_csc13009.nowwakealarm.databinding.ActivityAddAlarmBinding;
+import com.hcmus_csc13009.nowwakealarm.databinding.FragmentSettingsBinding;
 
 public class SettingsFragment extends Fragment {
+
+    private FragmentSettingsBinding fragmentSettingsBinding;
+    private String tone;
+    private Ringtone ringtone;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_settings, container, false);
+        fragmentSettingsBinding = FragmentSettingsBinding.inflate(inflater, container, false);
+        View view = fragmentSettingsBinding.getRoot();
+        tone = RingtoneManager.getActualDefaultRingtoneUri(this.getContext(), RingtoneManager.TYPE_ALARM).toString();
+        ringtone = RingtoneManager.getRingtone(getContext(), Uri.parse(tone));
+        fragmentSettingsBinding.alarmSoundCard.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RingtoneManager.ACTION_RINGTONE_PICKER);
+                intent.putExtra(RingtoneManager.EXTRA_RINGTONE_TYPE, RingtoneManager.TYPE_ALARM);
+                intent.putExtra(RingtoneManager.EXTRA_RINGTONE_TITLE, "Select Alarm Sound");
+                intent.putExtra(RingtoneManager.EXTRA_RINGTONE_EXISTING_URI, (Uri) Uri.parse(tone));
+                startActivityForResult(intent, 5);
+            }
+        });
+        fragmentSettingsBinding.setRadius.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                String radius = fragmentSettingsBinding.radius.getText().toString();
+                
+
+            }
+        });
+        return view;
+
     }
 }
